5958d6058ab89f5fc2d4eb0b9e6e7c6442d1174a
alclsth4456 <166373176+alclsth4456@users.noreply.github.com>
2024-06-02 02:06:56 +0900

    collection

---
+++ java/src/Collection/Ex11_5.java
@@ -0,0 +1,56 @@
+package Collection;
+
+import java.util.HashSet;
+import java.util.Objects;
+
+class SutdaCard {
+	int num;
+	boolean isKwang;
+	
+	SutdaCard() {
+		this(1,true);
+	}
+	SutdaCard(int num, boolean isKwang) {
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	
+	
+@Override
+	public int hashCode() {
+		return Objects.hash(isKwang, num);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof SutdaCard) {
+			SutdaCard c = (SutdaCard) obj;
+			
+			return isKwang == c.isKwang && num == c.num;
+		}else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return num + (isKwang ? "k" : " ");
+	}
+
+}
+public class Ex11_5 {
+
+	public static void main(String[] args) {
+		SutdaCard c1 = new SutdaCard(3,true);
+		SutdaCard c2 = new SutdaCard(3,true);
+		SutdaCard c3 = new SutdaCard(1,true);
+
+		HashSet set = new HashSet();
+		set.add(c1);
+		set.add(c2);
+		set.add(c3);
+		
+		System.out.println(set);
+	}
+
+}
+
+
