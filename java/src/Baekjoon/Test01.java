f08f8c52da215af868f238f2c5102622c4f66029
kimsj5847 <simsj0413@naver.com>
2024-07-04 12:10:05 +0900

    1

--- java/src/Baekjoon/Test01.java
+++ java/src/Baekjoon/Test01.java
@@ -21,7 +21,7 @@ public class Test01 {
 //		
 //		System.out.println(A%B);
 //		
-//		
+//	
 //		System.out.println("아이디 입력");
 //		String str = sc.nextLine();
 //		
@@ -31,7 +31,7 @@ public class Test01 {
 //		int year = sc.nextInt();
 //		
 //		System.out.println(year - 543);
-		
+//		
 //		int A = sc.nextInt();
 //		int B = sc.nextInt();
 //		int C = sc.nextInt();
@@ -40,35 +40,28 @@ public class Test01 {
 //		System.out.println((A%C) + (B%C)%C);
 //		System.out.println((A*B)%C);
 //		System.out.println(((A%C) * (B%C))%C);
-		
-		
-		
-		
-//		int arr1 []= new int [3];
-//		int result;
-//		int arr2 [] = new int [3];
-//		for(int i = 0; i < arr1.length; i++) {
-//			for(int j = 0; j < arr1)
-//			int a = sc.nextInt();
-//			int b = sc.nextInt();
-//			
-//		
-//		}
-		
-//		int a = sc.nextInt();
-//		String b = sc.nextLine();
-//		
-//		System.out.println(a * b.charAt(0)-'0');
-//		System.out.println(a * b.charAt(1)-'0');
-//		System.out.println(a * b.charAt(2)-'0');
-//		System.out.println(a * Integer.parseInt(b));
-	
-//		long a = sc.nextInt();
-//		long b = sc.nextInt();
-//		long c = sc.nextInt();
 //		
 //		
-//		System.out.println(a + b + c);
+//		
+//		
+
+		//2258
+		int a = sc.nextInt();
+		String b = sc.nextLine();
+		
+		System.out.println(a * b.charAt(0)-'0');
+		System.out.println(a * b.charAt(1)-'0');
+		System.out.println(a * b.charAt(2)-'0');
+		System.out.println(a * Integer.parseInt(b));
+
+		
+		//11382
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		long c = sc.nextInt();
+		
+		
+		System.out.println(a + b + c);
 	
 		
 		sc.close();
