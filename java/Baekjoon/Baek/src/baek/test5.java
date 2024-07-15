a04a81031943b407edf6f9b7843b008210ab3821
= <kimmsj5847@naver.com>
2024-07-15 19:08:13 +0900

    add

---
+++ java/Baekjoon/Baek/src/baek/test5.java
@@ -0,0 +1,101 @@
+package baek;
+
+import java.util.Scanner;
+
+public class teast5 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+//		String str = sc.nextLine();
+//		int n = sc.nextInt();
+//		char ch = str.charAt(n-1);
+//		System.out.println(ch);
+		
+		//2743
+//		String str = sc.nextLine();
+//		char[] ch = str.toCharArray();
+//		int n = ch.length;
+//		System.out.println(n);
+		
+		//9086
+//		int n = sc.nextInt();
+//		String arr [] = new String [n];
+//		
+//		for(int i = 0; i < n; i++) {
+//			String str = sc.next();
+//			System.out.println(str.charAt(0));
+//			System.out.println(str.charAt(str.length()-1));
+//		}
+		
+		//11654
+//		String str = sc.next();
+//		int n = (int)str.charAt(0);
+//		System.out.println(n);
+
+//		int num = sc.next().charAt(0);
+//		System.out.println(num);
+		
+		//11720
+//		int n = sc.nextInt();
+//		int sum = 0;
+//		String str = sc.next();
+//		for(int i = 0; i < n; i++) {
+//			
+//			sum += str.charAt(i)-'0';
+//		}
+//		System.out.println(sum);
+		
+		//10809
+//		String  str = sc.next();
+//		int arr [] = new int [26];
+//		
+//		
+//		for(int i = 0; i < 26; i++) {
+//			arr[i] = -1;
+//		}
+//		
+//		for(int i = 0; i < str.length(); i++) {
+//			char ch = str.charAt(i);
+//		
+//			if(arr[ch-'a'] == -1) {
+//				arr[ch-'a'] = i;
+//		}
+//		}
+//		
+//		for(int a : arr) {
+//			System.out.println(a);
+//		}
+//		
+		
+		//2675
+//		System.out.println("테스트 갯수");
+//		int t = sc.nextInt();//테스트 갯수
+//		String arr [] = new String [t];
+//		
+//		for(int i = 0; i < t; i++) {			
+//			System.out.println("문자열 반복할 갯수");
+//			int r = sc.nextInt();  //문자열 반복할 갯수
+//			System.out.println("스트링 입력");
+//			String s = sc.next();  //스트링 입력
+//	
+//			for(int j = 0; j < s.length(); j ++) {
+//				for(int k = 0; k < r; k++) {
+//					System.out.print(s.charAt(j));
+//					
+//				}
+//			}
+//			System.out.println();
+//			
+//		}
+		
+		//1152 
+		
+		
+		}
+		
+	
+
+}
