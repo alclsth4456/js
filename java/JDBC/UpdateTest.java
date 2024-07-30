3899c80c8031c9b3ceb9a79d7594d0a64aab4e31
kimsj5847 <kimmsj5847@naver.com>
2024-07-31 00:41:17 +0900

    statement

---
+++ java/JDBC/UpdateTest.java
@@ -0,0 +1,23 @@
+package sub3;
+
+public class UpdateTest {
+
+	public static void main(String[] args) {
+
+		//db정보
+				String host = "jdbc:mysql://127.0.0.1:3306/studydb";
+				String user = "root";
+				String pass = "kimsj1234";
+				
+				try {
+					//1단계- 데이터베이스 접속
+					//2단계- SQL 실행 객체 (PreparedStatement) 생성 후 매핑
+					//3단계- SQL 실행
+					// 4단계 - ResultSet 결과처리
+					// 5단계 - 데이터베이스 종료
+					}catch(Exception e) {
+					
+				}
+	}
+
+}
